c185b935d8aba595d6af084433bc3b2a09e25641
PhoenixmitX <hauke.weinert@gmail.com>
2023-02-08 20:13:37 +0100

    make StaticDelegateTransformer less confusing

--- src/codegen/java/de/phoenixmitx/lombokextensions/codegen/delegate/StaticDelegateTransformer.java
+++ src/codegen/java/de/phoenixmitx/lombokextensions/codegen/delegate/StaticDelegateTransformer.java
@@ -17,7 +17,8 @@ import javassist.bytecode.annotation.ClassMemberValue;
 import javassist.bytecode.annotation.MemberValue;
 
 public class StaticDelegateTransformer implements ClassFileTransformer {
-  private final ClassPool pool = ClassPool.getDefault();
+
+  private final ClassPool classPool = ClassPool.getDefault();
 
   @Override
   public byte[] transform(ClassLoader loader, String className, Class<?> classBeingRedefined, ProtectionDomain protectionDomain, byte[] classfileBuffer) throws IllegalClassFormatException {
@@ -26,48 +27,47 @@ public class StaticDelegateTransformer implements ClassFileTransformer {
     }
     try {
       // Get the class using javassist
-      CtClass clazz = pool.get(className.replace("/", "."));
+      CtClass newClass = classPool.get(className.replace("/", "."));
 
       // Check if the class has the StaticDelegate annotation
-      AnnotationsAttribute attr = (AnnotationsAttribute) clazz.getClassFile().getAttribute(AnnotationsAttribute.visibleTag);
-      if (attr == null) {
+      AnnotationsAttribute annotationsAttribute = (AnnotationsAttribute) newClass.getClassFile().getAttribute(AnnotationsAttribute.visibleTag);
+      if (annotationsAttribute == null) {
         return classfileBuffer;
       }
-      Annotation annotation = attr.getAnnotation(StaticDelegate.class.getName());
-      if (annotation != null) {
-        // Get the classes to delegate to from the annotation
-        MemberValue value = annotation.getMemberValue("value");
-        ArrayMemberValue classes = (ArrayMemberValue) value;
-        MemberValue[] classValues = classes.getValue();
+      Annotation annotation = annotationsAttribute.getAnnotation(StaticDelegate.class.getName());
+      if (annotation == null) {
+        return classfileBuffer;
+      }
+      // Get the classes to delegate to from the annotation
+      MemberValue[] classValues = ((ArrayMemberValue) annotation.getMemberValue("value")).getValue();
 
-        // Delegate all static methods from the delegated classes to the annotated class
-        for (MemberValue classValue : classValues) {
-          System.out.println("Delegating static methods from " + classValue + " to " + className);
-          ClassMemberValue delegateClass = (ClassMemberValue) classValue;
-          String delegateClassName = delegateClass.getValue();
-          CtClass delegate = pool.get(delegateClassName);
+      // Delegate all static methods from the delegated classes to the annotated class
+      for (MemberValue classValue : classValues) {
+        System.out.println("Delegating static methods from " + classValue + " to " + className);
+        String oldClassName = ((ClassMemberValue) classValue).getValue();
+        CtClass oldClass = classPool.get(oldClassName);
 
-          for (CtMethod method : delegate.getDeclaredMethods()) {
+        for (CtMethod oldMethod : oldClass.getDeclaredMethods()) {
 
-            // Check if the method is static
-            if (Modifier.isStatic(method.getModifiers()) && Modifier.isPublic(method.getModifiers())) {
+          // Check if the method is static
+          if (Modifier.isStatic(oldMethod.getModifiers()) && Modifier.isPublic(oldMethod.getModifiers())) {
 
-              CtMethod newMethod = new CtMethod(method.getReturnType(), method.getName(), method.getParameterTypes(), clazz);
-              newMethod.setModifiers(Modifier.PUBLIC | Modifier.STATIC);
-              if (method.getGenericSignature() != null) newMethod.setGenericSignature(method.getGenericSignature());
-              newMethod.setBody("return " + delegateClassName + "." + method.getName() + "($$);");
-              clazz.addMethod(newMethod);
-            }
+            CtMethod newMethod = new CtMethod(oldMethod.getReturnType(), oldMethod.getName(), oldMethod.getParameterTypes(), newClass);
+            newMethod.setModifiers(Modifier.PUBLIC | Modifier.STATIC);
+            if (oldMethod.getGenericSignature() != null) newMethod.setGenericSignature(oldMethod.getGenericSignature());
+            newMethod.setBody("return " + oldClassName + "." + oldMethod.getName() + "($$);");
+
+            newClass.addMethod(newMethod);
           }
         }
-
-        // Remove the StaticDelegate annotation
-        attr.removeAnnotation("de.phoenixmitx.lombokextensions.codegen.delegate.StaticDelegate");
-
-        // Return the modified bytecode
-        clazz.getClassFile().write(new DataOutputStream(new FileOutputStream("build/classes/java/main/" + className + ".class")));
-        return clazz.toBytecode();
       }
+
+      // Remove the StaticDelegate annotation
+      annotationsAttribute.removeAnnotation("de.phoenixmitx.lombokextensions.codegen.delegate.StaticDelegate");
+
+      // Return the modified bytecode
+      newClass.getClassFile().write(new DataOutputStream(new FileOutputStream("build/classes/java/main/" + className + ".class")));
+      return newClass.toBytecode();
     } catch (Exception e) {
       IllegalClassFormatException ifx = new IllegalClassFormatException("Error transforming class " + className);
       ifx.initCause(e);
@@ -75,6 +75,5 @@ public class StaticDelegateTransformer implements ClassFileTransformer {
       e.printStackTrace();
       throw ifx;
     }
-    return classfileBuffer;
   }
 }
