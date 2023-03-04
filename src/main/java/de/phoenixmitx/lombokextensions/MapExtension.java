0c9531f193836805153517be99b22c4450d2706e
PhoenixmitX <hauke.weinert@gmail.com>
2023-03-04 23:01:32 +0100

    add mapValues

--- src/main/java/de/phoenixmitx/lombokextensions/MapExtension.java
+++ src/main/java/de/phoenixmitx/lombokextensions/MapExtension.java
@@ -1,7 +1,10 @@
 package de.phoenixmitx.lombokextensions;
 
+import java.util.Collections;
+import java.util.HashMap;
 import java.util.Map;
 import java.util.Optional;
+import java.util.function.Function;
 
 import lombok.experimental.UtilityClass;
 
@@ -10,5 +13,24 @@ public class MapExtension {
 	
 	public <K,V> Optional<V> find(Map<K,V> map, K key) {
 		return Optional.ofNullable(map.get(key));
+	}
+
+	public static <K, O, N> Map<K, N> mapValues(Map<K, O> map, Function<O, N> mapper) {
+		return mapValues(map, mapper, new HashMap<>(), false);
+	}
+	
+	public static <K, O, N> Map<K, N> mapValues(Map<K, O> map, Function<O, N> mapper, Map<K, N> newMap) {
+		return mapValues(map, mapper, newMap, false);
+	}
+
+	public static <K, O, N> Map<K, N> mapValues(Map<K, O> map, Function<O, N> mapper, boolean makeImmutable) {
+		return mapValues(map, mapper, new HashMap<>(), makeImmutable);
+	}
+
+	public static <K, O, N> Map<K, N> mapValues(Map<K, O> map, Function<O, N> mapper, Map<K, N> newMap, boolean makeImmutable) {
+		for (Map.Entry<K, O> entry : map.entrySet()) {
+			newMap.put(entry.getKey(), mapper.apply(entry.getValue()));
+		}
+		return makeImmutable ? Collections.unmodifiableMap(newMap) : newMap;
 	}
 }
