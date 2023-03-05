56e783a2f0a848ae136e6f14bf41d21591cbbdf1
PhoenixmitX <hauke.weinert@gmail.com>
2023-03-05 19:13:41 +0100

    add mapValues with key

--- src/main/java/de/phoenixmitx/lombokextensions/MapExtension.java
+++ src/main/java/de/phoenixmitx/lombokextensions/MapExtension.java
@@ -4,6 +4,7 @@ import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Optional;
+import java.util.function.BiFunction;
 import java.util.function.Function;
 
 import lombok.experimental.UtilityClass;
@@ -30,6 +31,25 @@ public class MapExtension {
 	public static <K, O, N> Map<K, N> mapValues(Map<K, O> map, Function<O, N> mapper, Map<K, N> newMap, boolean makeImmutable) {
 		for (Map.Entry<K, O> entry : map.entrySet()) {
 			newMap.put(entry.getKey(), mapper.apply(entry.getValue()));
+		}
+		return makeImmutable ? Collections.unmodifiableMap(newMap) : newMap;
+	}
+
+	public static <K, O, N> Map<K, N> mapValues(Map<K, O> map, BiFunction<K, O, N> mapper) {
+		return mapValues(map, mapper, new HashMap<>(), false);
+	}
+	
+	public static <K, O, N> Map<K, N> mapValues(Map<K, O> map, BiFunction<K, O, N> mapper, Map<K, N> newMap) {
+		return mapValues(map, mapper, newMap, false);
+	}
+
+	public static <K, O, N> Map<K, N> mapValues(Map<K, O> map, BiFunction<K, O, N> mapper, boolean makeImmutable) {
+		return mapValues(map, mapper, new HashMap<>(), makeImmutable);
+	}
+
+	public static <K, O, N> Map<K, N> mapValues(Map<K, O> map, BiFunction<K, O, N> mapper, Map<K, N> newMap, boolean makeImmutable) {
+		for (Map.Entry<K, O> entry : map.entrySet()) {
+			newMap.put(entry.getKey(), mapper.apply(entry.getKey(), entry.getValue()));
 		}
 		return makeImmutable ? Collections.unmodifiableMap(newMap) : newMap;
 	}
