0429ce6b26706fdddb019d9662ec0a3aa0d938ff
PhoenixmitX <hauke.weinert@gmail.com>
2023-03-18 15:29:53 +0100

    add find method to list extension

--- src/main/java/de/phoenixmitx/lombokextensions/ListExtension.java
+++ src/main/java/de/phoenixmitx/lombokextensions/ListExtension.java
@@ -1,6 +1,9 @@
 package de.phoenixmitx.lombokextensions;
 
 import java.util.Collection;
+import java.util.Optional;
+import java.util.function.Function;
+import java.util.function.Predicate;
 
 import lombok.experimental.UtilityClass;
 
@@ -9,5 +12,18 @@ public class ListExtension {
 	
 	public <T> boolean isIn(T t, Collection<T> collection) {
 		return collection.contains(t);
+	}
+
+	public <T> Optional<T> find(Collection<T> collection, Predicate<T> condition) {
+		for (T t : collection) {
+			if (condition.test(t)) {
+				return Optional.of(t);
+			}
+		}
+		return Optional.empty();
+	}
+
+	public <T, O> Optional<T> find(Collection<T> collection, Function<T, O> function, O o) {
+		return find(collection, t -> o.equals(function.apply(t)));
 	}
 }
